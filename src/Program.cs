f3c526f79d2222bd9fbafb7389b6e62511484b70
David Cheatham <sancrock@gmail.com>
2020-01-21 18:36:22 -0500

    Added correct scaffold

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _YourName_.CodeLou.ExerciseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
